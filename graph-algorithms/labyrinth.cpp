e1db7b35275b0a6a002e4733d645be99ef9bc826
nothuman <nothuman2.718@gmail.com>
2025-09-12 15:04:30 +0530

    GA: Labyrinth 12-09-25
    [12-09-25]

---
+++ graph-algorithms/labyrinth.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define vc vector<char>
+#define vvc vector<vector<char>>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define sll set<long long>
+#define usi unordered_set<int>
+#define usc unordered_set<char>
+#define usll unordered_set<long long>
+
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+// /* clang-format on */
+// void dfs(vvc& grid, int R, int C, int r,int c,string& s, vector<string>& str){
+//     if(grid[r][c]=='B') {
+//         str.pb(s);
+//     }
+//     grid[r][c]='0';
+//     int dRow[]={-1,0,1,0};
+//     int dCol[]={0,1,0,-1};
+//     char moves[]={'U','R','D','L'};
+    
+//     f(i,0,4){
+//         int newR = dRow[i]+r;
+//         int newC = dCol[i]+c;
+//         if(newR<0 || newR >=R || newC>= C || newC<0 
+//                 || grid[newR][newC]=='#' || grid[newR][newC]=='0')
+//             continue;
+//         s+=moves[i];
+//         dfs(grid,R,C,newR,newC,s,str);
+//     }
+//     grid[r][c]='.';
+// }
+
+/* Main()  function */
+int main()
+{
+
+	int R,C;
+    cin >> R;
+    cin >> C;
+
+    vvc grid(R, vc (C,'0'));
+
+    f(i,0,R){
+        f(j,0,C){
+            cin >> grid[i][j];
+        }
+    }
+    string s="";
+    int row=0;
+    int col=0;
+    f(i,0,R){
+        f(j,0,C){
+            if(grid[i][j]=='A'){
+                row=i; col=j;
+            }
+        }
+    }
+
+    bool found = false;
+    queue<pair<int,int>> q;
+    q.push({row,col});
+
+    while(!q.empty()){
+        auto [r,c] = q.front(); q.pop();
+
+        int dRow[]={-1,0,1,0};
+        int dCol[]={0,1,0,-1};
+        char moves[]={'U','R','D','L'};
+
+        f(i,0,4){
+            int newR = dRow[i]+r;
+            int newC = dCol[i]+c;
+            if(newR<0 || newR >=R || newC>= C || newC<0)
+                continue;
+            if(grid[newR][newC]=='B'){
+                grid[newR][newC]=moves[i];
+                found=true;
+                row=newR;
+                col=newC;
+                break;
+            }
+            if(grid[newR][newC]=='.'){
+                grid[newR][newC]=moves[i];
+                q.push({newR,newC});
+            }
+            
+        }
+        if(found) break;
+    }
+
+    if(found){
+        cout<<"YES\n";
+        while(grid[row][col]!='A'){
+            char move = grid[row][col];
+            s+=move;
+            if(move=='U') row+=1;
+            if(move=='R') col-=1;
+            if(move=='D') row-=1;
+            if(move=='L') col+=1;
+        }
+        reverse(s.begin(),s.end()); 
+    
+        cout<< s.size() <<endl;
+        cout<<s<<'\n';
+    }
+    else cout<<"NO\n";
+
+   return 0;
+}
+/* Main() Ends Here */
